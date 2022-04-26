6b9317aa5fef3d6c64662adb40b62c1b0cded6b3
Felipe Sousa <felipe.fps09@hotmail.com>
2022-04-26 20:41:08 -0300

    Tests ok

---
+++ src/main/java/com/devsuperior/bds04/services/EventService.java
@@ -0,0 +1,45 @@
+package com.devsuperior.bds04.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.bds04.dto.EventDTO;
+import com.devsuperior.bds04.entities.City;
+import com.devsuperior.bds04.entities.Event;
+import com.devsuperior.bds04.repository.EventRepository;
+
+@Service
+public class EventService {
+
+	@Autowired
+	EventRepository repository;
+
+	@Transactional(readOnly = true)
+	public Page<EventDTO> findAllPaged(Pageable pageable) {
+		PageRequest request = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), Sort.by("name"));
+		Page<Event> page = repository.findAll(request);
+		return page.map(x -> new EventDTO(x));
+	}
+
+	@Transactional
+	public EventDTO insert(EventDTO dto) {
+		Event entity = new Event();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new EventDTO(entity);
+	}
+
+	private void copyDtoToEntity(EventDTO dto, Event entity) {
+		entity.setName(dto.getName());
+		entity.setDate(dto.getDate());
+		entity.setUrl(dto.getUrl());
+		entity.setCity(new City(dto.getCityId(), null));
+
+	}
+
+}
